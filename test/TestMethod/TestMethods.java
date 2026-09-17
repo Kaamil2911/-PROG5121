d4af7df019ac0bd21bc1e0459097c25f06402155
Kaamil Mokomela <kaamil2911@gmail.com>
2026-09-17 14:02:12 +0200

    Junit tests for correct and incorrect password and username validation

--- test/TestMethod/TestMethods.java
+++ test/TestMethod/TestMethods.java
@@ -1,25 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 package TestMethod;
 
 import login.Login;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test; 
+import static org.junit.Assert.*;
 
 /**
- *
  * @author kaami
  */
 public class TestMethods {
     
     @Test
-    public void TestUserName(){
-    Login name = new Login();
+    public void testUserName() {
+        String testInput = "kyl_1";
+        boolean actual = Login.checkUserName(testInput);
+        assertEquals("EXPECT CORRECT FORMAT", true, actual);
+        
+        
+    }
+    @Test
+    public void testIncorrectUserName() {
+        String testInput = "kyle!!!!!";
+        boolean actual = Login.checkUserName(testInput);
+        assertEquals("Username is not correctly formatted; please ensure that your username contains an underscore and no more than 5 characters in length", true, actual);
+        
+        
+    }
     
-    
-    
-}
-    
+    @Test
+    public void testPassword() {
+        String testInput = "Ch&&sec@ke99";
+        boolean actual = Login.checkPasswordComplexity(testInput);
+        assertEquals("EXPECT CORRECT FORMAT", true, actual);
+        
+        
+    }
+    @Test
+    public void testIncorrectPassword() {
+        String testInput = "password";
+        boolean actual = Login.checkPasswordComplexity(testInput);
+        assertEquals("Password is not correctly formatted; please ensure that the pasword contains at least eight chracters, a capital letter, a number and a special chracter.", true, actual);
+        
+        
+    }
+
+   
 }
