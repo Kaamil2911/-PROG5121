d3457ef6e4ec3e336e61769822a3d1595c893706
Kaamil Mokomela <kaamil2911@gmail.com>
2026-09-17 10:09:01 +0200

    added user variables for account creation

--- src/login/Login.java
+++ src/login/Login.java
@@ -1,22 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package login;
 
-/**
- *
- * @author kaami
- */
+// Scanner library for user input
+import java.util.Scanner;
+
 public class Login {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        String username = "Kaamil";
+     static void main(String[] args) {
+        
+        // object made to scan user input
+        Scanner input = new Scanner(System.in);
+        
+        String userName = "";
+        String passWord = "";
+        String phoneNumber = "";
+        
+        System.out.println("""
+                           Welcome User !
+                           You are required to Create an Account
+                           Please do the following:
+                           """);
+        System.out.println("Enter a username: ");
+        userName = input.nextLine();
+        
+        System.out.println("Enter a password: ");
+        passWord = input.nextLine();
+        
+        System.out.println("Enter a phone number: ");
+        phoneNumber = input.nextLine();
         
         
+    
     }
     
 }
